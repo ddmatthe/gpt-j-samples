239118f784de9dce45178c213406d669476d736c
Dennis Matthews <dennis.d.matthews@intel.com>
2024-03-14 00:38:19 -0700

    works on baremetal and in container using the run.sh

--- bare-metal/simple-gpt-j.py
+++ bare-metal/simple-gpt-j.py
@@ -3,16 +3,7 @@ import sys
 import torch
 from transformers import GPTJForCausalLM, AutoTokenizer
 
-model_loaded_flag = False
-if(len(sys.argv) > 1):
-   model_path = os.path.join(sys.argv[1])
-   if( os.path.exists( model_path )):
-      print(model_path)
-      model = GPTJForCausalLM.from_pretrained( model_path, revision="float32",  torch_dtype=torch.float32)
-      model_loaded_flag = True
-if(model_loaded_flag == False):
-    model = GPTJForCausalLM.from_pretrained( "EleutherAI/gpt-j-6b", revision="float32",  torch_dtype=torch.float32)
-    model.save_pretrained(model_path)
+model = GPTJForCausalLM.from_pretrained( "EleutherAI/gpt-j-6b", revision="float32",  torch_dtype=torch.float32)
 prompt = "To make a great scrambled egg, you must"
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6b", trust_remote_code=True)
 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
