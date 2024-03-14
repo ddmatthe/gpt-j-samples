5ac799fc7262761cc279925ae53664aae35801f4
Dennis Matthews <dennis.d.matthews@intel.com>
2024-03-14 16:25:36 -0700

    adding in home made dockerfile that builds pytorch from scratch

--- bare-metal/simple-gpt-j.py
+++ bare-metal/simple-gpt-j.py
@@ -4,7 +4,7 @@ import torch
 from transformers import GPTJForCausalLM, AutoTokenizer
 
 model = GPTJForCausalLM.from_pretrained( "EleutherAI/gpt-j-6b", revision="float32",  torch_dtype=torch.float32)
-prompt = "To make a great scrambled egg, you must"
+prompt = "To make a great taco, you must"
 tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6b", trust_remote_code=True)
 input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.9, max_length=400)
